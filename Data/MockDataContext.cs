24f873fd3effdbadfaf90b79c0cd55ea63c533fa
Chirag Baroliya <chiragb@devrepublic.nl>
2026-05-20 15:54:29 +0530

    Add new Designation in mock file

--- Data/MockDataContext.cs
+++ Data/MockDataContext.cs
@@ -14,7 +14,8 @@ namespace AngularApp.BackendAPI.Data
         {
             new Designation { Id = 1, Name = "Software Engineer" },
             new Designation { Id = 2, Name = "Manager" },
-            new Designation { Id = 3, Name = "HR" }
+            new Designation { Id = 3, Name = "HR" },
+            new Designation { Id = 4, Name = "Developer" },
         };
 
         public static List<Gender> Genders { get; set; } = new List<Gender>
